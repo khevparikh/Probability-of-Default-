6983972d2f563eb9d2203792445608164d421d54
Adeet Patel <adeet.patel@gmail.com>
2022-11-22 02:50:19 -0500

    Create train_harness and predict_harness functions, rearchitect codebase

--- CorporateDefaultModel.py
+++ CorporateDefaultModel.py
@@ -13,70 +13,32 @@ from sklearn.metrics import roc_auc_score
 
 class CorporateDefaultModel:
     def __init__(self):
+        # Store models here during walk-forward analysis
+        self.models = None
         return
     
-    def load_data(self, df):
-        self.df = df
-        print("Raw data:", self.df.shape)
-    
-    def preprocess_data(self):
-        """
-        This function ...
-        
-        
-        """
+    def preprocess_data(self, data):
         # pandas indexes the dataframe automatically, so we can remove the Unnamed column.
         # We remove the column eqty_corp_family_tot because it is all NaNs.
         # Drop statement date because fs_year already contains that information
         # All statement dates are 12/31, which doesn't tell us anything new
         
-        self.df.drop(columns = ['Unnamed: 0', 'eqty_corp_family_tot', 'stmt_date'], inplace=True)
+        data.drop(columns = ['Unnamed: 0', 'eqty_corp_family_tot', 'stmt_date'], inplace=True)
         
         # Sort the data by company ID and fiscal year to prevent potential look-ahead bias
-        self.df.sort_values(["fs_year", "id"], inplace=True)
-        self.df.reset_index(drop=True, inplace=True)
+        data.sort_values(["fs_year", "id"], inplace=True)
+        data.reset_index(drop=True, inplace=True)
         
-             
         # Cast default date to datetime type
-        self.df["def_date"] = pd.to_datetime(self.df["def_date"])
+        data["def_date"] = pd.to_datetime(data["def_date"])
         
         # Cast ATECO sector from float to integer
-        df["ateco_sector"] = df["ateco_sector"].astype(int)
+        data["ateco_sector"] = data["ateco_sector"].astype(int)
         
         # TO-DO: preprocess industry ATECO sector (look at the manuals Roger gave us)
         pass
     
         # Convert default dates into indicator targets
-        self.target = self.date_to_target(df["def_date"])
-        
-        """
-        Because the (fiscal year, company ID) pair uniquely identifies each row, make
-        it the index.
-        """
-        self.df.set_index(["fs_year", "id"], inplace=True)
-        
-        print("Number of companies in dataset:", self.df.index.map(lambda pair: pair[1]).nunique())
-        
-        # Dimensions of the new preprocessed data
-        print("Preprocessed data:", self.df.shape)
-    
-    # Function for imputing (filling in NaN values)
-    def impute(self):
-        # Fields to impute
-        fields = ['wc_net', 'asst_tot', 'ebitda', 'eqty_tot', 'cf_operations', 'taxes', \
-                  'debt_st', 'debt_lt', 'debt_bank_lt', 'liab_lt', 'liab_lt_emp', 'AP_lt', \
-                  'roe', 'roa', 'prof_financing', 'exp_financing']
-        print("Number of fields to impute:", len(fields))
-        
-        company_index = self.df.index.map(lambda pair: pair[1])
-        companies = np.unique(company_index)
-            
-        # Impute NaNs based on interpolation method
-        map(lambda company: self.df.loc[company_index == company, fields].interpolate(
-            limit_direction="forward", inplace=True), companies)
-    
-    # Function for encoding default dates into 1s and 0s
-    def date_to_target(self, def_dates):        
         """
         In Italy, the fiscal period is 1/1 to 12/31. We assume that we sit
         down 1/1 and run the model to predict default probabilities.
@@ -95,69 +57,72 @@ class CorporateDefaultModel:
         the moment that y_t changes from 0 to 1, it will always remain 1.
         """
         
-        result = np.logical_and(def_dates.notna(),
-                                self.df["fs_year"] + 1 >= def_dates.dt.year).astype(int)
+        data["target"] = np.logical_and(data["def_date"].notna(),
+                                        data["fs_year"] + 1 >= data["def_date"].dt.year).astype(int)
         
-        target_index = pd.MultiIndex.from_frame(self.df[["fs_year", "id"]])
-        return pd.Series(result.values, index=target_index)
+        """
+        Because the (fiscal year, company ID) pair uniquely identifies each row, make
+        it the index.
+        """
+        data.set_index(["fs_year", "id"], inplace=True)
+        
+        print("Number of companies in dataset:", data.index.map(lambda pair: pair[1]).nunique())
     
-    def engineer_features(self):
+    # Function for imputing (filling in NaN values)
+    def impute(self, data):
+        # Fields to impute
+        fields = ['wc_net', 'asst_tot', 'ebitda', 'eqty_tot', 'cf_operations', 'taxes', \
+                  'debt_st', 'debt_lt', 'debt_bank_lt', 'liab_lt', 'liab_lt_emp', 'AP_lt', \
+                  'roe', 'roa', 'prof_financing', 'exp_financing']
+        print("Number of fields to impute:", len(fields))
+        
+        company_index = data.index.map(lambda pair: pair[1])
+        companies = np.unique(company_index)
+        
+        # Impute NaNs based on interpolation method
+        map(lambda company: data.loc[company_index == company, fields].interpolate(
+            limit_direction="forward", inplace=True), companies)
+    
+    def engineer_features(self, data):
+        # Save the target in its own variable
+        target = data["target"]
+        
         # Create a dataframe of features that will be used by the model
-        self.features = pd.DataFrame([], index=self.df.index)
+        features = pd.DataFrame([], index=data.index)
         
         # Calculate features
-        self.features["wc_ta"] = self.df.wc_net / self.df.asst_tot
-        self.features["ebit_ta"] = self.df.ebitda / self.df.asst_tot
-        self.features["leverage"] = 1 - self.df.eqty_tot / self.df.asst_tot
-        self.features["cf_to_debt"] = self.df.cf_operations / (self.df.debt_st + self.df.debt_lt)
-        self.features["log_AP_st"] = np.log(self.df.AP_st)
-        self.features["ST_debt_to_cur_asst"] = self.df.debt_st / self.df.asst_current
+        features["wc_ta"] = data.wc_net / data.asst_tot
+        features["ebit_ta"] = data.ebitda / data.asst_tot
+        features["leverage"] = 1 - data.eqty_tot / data.asst_tot
+        features["cf_to_debt"] = data.cf_operations / (data.debt_st + data.debt_lt)
+        features["log_AP_st"] = np.log(data.AP_st)
+        features["ST_debt_to_cur_asst"] = data.debt_st / data.asst_current
         
         """
         Suggested by Khevna - she found a 16% difference in median profit
         between defaulting and nondefaulting firms.
         """
-        self.features["financial_profit"] = self.df.prof_financing
+        features["financial_profit"] = data.prof_financing
         
         # TODO: We can't just delete rows, we need to deal with those companies
         # in specific ways. We must output a probability for every company.
         #
         # The mask is an array of booleans, such that if mask[i] = True, it means
         # we delete row i.
-        self.features.replace([np.inf, -np.inf], np.nan, inplace=True)
-        is_inf = np.isinf(self.features)
-        is_nan = np.isnan(self.features)
+        features.replace([np.inf, -np.inf], np.nan, inplace=True)
+        is_inf = np.isinf(features)
+        is_nan = np.isnan(features)
 
-        self.features.ffill(axis=0, inplace=True)
+        features.ffill(axis=0, inplace=True)
         mask = np.any(np.logical_or(is_inf, is_nan), axis=1)
         reordered_mask=pd.DataFrame(mask).reorder_levels(["id", "fs_year"]).sort_index()
         for col in list(reordered_mask.columns):
             reordered_mask[col]=reordered_mask.groupby('id')[col].transform(lambda x: x.ffill())
-
-        print(reordered_mask)
-        #Impute the value for the specific company id; idea: forward fill (from the previous year)? 
-        #Reorder index as id, fs_year to do this
         
-        #print('line 136:', reordered_mask)
-        
-        #print("Dropping {} rows".format(np.count_nonzero(mask)))
-        
-        # Now that we have features for our model, we don't need the original data anymore
-        self.df = None
-
-        #include this after engineer_features function
-
-#def transformation_ratio(self):    
-#self.df
-#N=len(df_afterengineerfeatures) #this is after dropping the isna or isinf values 
-#ateco_transf=train.sort_values(by='leverage')
-#ateco_transf
-#estimate nonlinear curve
-#Define k buckets
-#Define a nonlinear curve to map quantiles
+        return features, target
 
     # Walk-forward analysis
-    def train(self):
+    def train(self, features, target):
         """
         Walk-forward algorithm:
             
@@ -166,34 +131,44 @@ class CorporateDefaultModel:
             3) Save the predictions in an array.
             4) Increment t by 1 and repeat steps 1 to 3.
         """
-        fs_year = self.features.index.map(lambda pair: pair[0])
+        fs_year = features.index.map(lambda pair: pair[0])
         self.years = np.unique(fs_year)[:-1] #list of all years besides last one for mapping purpose
         
         # Select part of the data for training
-        X_trains = map(lambda t: self.features[fs_year <= t], self.years)
-        y_trains = map(lambda t: self.target[fs_year <= t], self.years)
-        
+        X_trains = list(map(lambda t: features[fs_year <= t], self.years))
+        y_trains = list(map(lambda t: target[fs_year <= t], self.years))
+                
         # Train models
         self.models = map(lambda X, y: XGBClassifier().fit(X, y), X_trains, y_trains)
+        #self.models = list(map(lambda X, y: LogisticRegression().fit(X, y), X_trains, y_trains))
         
-        #self.models = map(lambda X, y: LogisticRegression().fit(X, y), X_trains, y_trains)
-        
+        # Save our training data to the class
+        self.train_features = features
+        self.train_target = target
+    
     # Make predictions using the trained models
-    def predict(self):
-        #self.features: dataframe of features that will be used by the model
-        fs_year = self.features.index.map(lambda pair: pair[0])
+    def predict(self, features, target):
+        if self.models == None:
+            raise Exception("Model not trained yet")
+        
+        #features: dataframe of features that will be used by the model
+        fs_year = features.index.map(lambda pair: pair[0])
         
         # Select part of the data for testing
-        X_tests = map(lambda t: self.features[fs_year == t+1], self.years)
-        y_tests = map(lambda t: self.target[fs_year == t+1], self.years)
+        X_tests = map(lambda t: features[fs_year == t+1], self.years)
+        X_tests = list(X_tests)
+        y_tests = map(lambda t: target[fs_year == t+1], self.years)
+        y_tests = list(y_tests)
         
         # Aggregate ground truth values for each company
         # For each company, the last value tells us whether the company ultimately defaulted
-        true = pd.concat(y_tests).reorder_levels(["id", "fs_year"]).sort_index()
+        true = pd.concat(y_tests)
+        true = true.reorder_levels(["id", "fs_year"]).sort_index()
         true = true.groupby(level="id").last()
         
         # Compute predictions for each row
         pred = map(lambda model, X: pd.DataFrame(model.predict_proba(X), index=X.index), self.models, X_tests)
+        pred = list(pred)
         pred = pd.concat(pred)
         
         # Aggregate these predictions to generate a default probability for each company
@@ -202,41 +177,52 @@ class CorporateDefaultModel:
         pred = pred.groupby(level="id").last()
         
         return true, pred
-
+        
     def calibration(self, pred):
-        #baseline default rate ranging from 0.5 to 15%
-        pi_sample=self.target.mean()
+        # ACPMIP p. 220
+        #
+        # Baseline default rate ranging from 0.5 to 15%
+        pi_sample = self.train_target.mean()
         print("Sample default rate:", pi_sample)
-        pi_true=0.005
-        #t,pred=self.predict()
-        pi_adjusted=pred.apply(lambda x: (pi_true/pi_sample)*(x))
-
-        #Elkan_calibration
-        pi_adjusted_el=pred.apply(lambda x:(pi_true)*(x - (x*pi_sample))/(pi_sample-(x*pi_sample)+(x*pi_true)-(pi_sample*pi_true)))
-
+        pi_true = 0.005
+        print("calibration : pred =\n", pred)
+        pi_adjusted = pred.apply(lambda x: (pi_true/pi_sample)*x)
+        
+        # Elkan_calibration
+        pi_adjusted_el = pred.apply(lambda x: (pi_true)*(x - (x*pi_sample))/(pi_sample-(x*pi_sample)+(x*pi_true)-(pi_sample*pi_true)))
+        
         return pi_adjusted
     
-    def harness(self):
-        self.preprocess_data()
-        self.impute()
-        self.engineer_features()
-        self.train()
-        t,p=self.predict()
-        pi_a=self.calibration(p)
-        return t, pi_a
+    # A function that consolidates all steps needed for training
+    def train_harness(self, train):
+        self.preprocess_data(train)
+        self.impute(train)
+        features, target = self.engineer_features(train)
+        
+        # Now that the data has been transformed, we can use it for training
+        self.train(features, target)
+    
+    # A function that consolidates all steps needed for testing
+    def predict_harness(self, test):
+        self.preprocess_data(test)
+        self.impute(test)
+        features, target = self.engineer_features(test)
+        
+        # Now that the data has been transformed, we can make predictions on it
+        true, pred = self.predict(features, target)
+        
+        # Calibrate the probabilities
+        return true, self.calibration(pred)
 
 print("Reading data...")
-path=r"/Users/anthonychen/Desktop/ML_Finance/train.csv"
-#path="train.csv"
+#path=r"/Users/anthonychen/Desktop/ML_Finance/train.csv"
+path="train.csv"
 df = pd.read_csv(path)
-
 print("Reading data... DONE")
 
+train = df.iloc[0:1_000_000, :]
+test = df.iloc[1_000_000:, :]
 model = CorporateDefaultModel()
-model.load_data(df)
-t, pi_adjusted= model.harness()
-
-X, y = model.features, model.target
-
-print("AUC =", roc_auc_score(t, pi_adjusted.iloc[:, 1]))
-
+model.train_harness(train)
+true, pi_adjusted = model.predict_harness(test)
+print("AUC =", roc_auc_score(true, pi_adjusted.iloc[:, 1]))
