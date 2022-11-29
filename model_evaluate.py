9755abca83c57553c99196e4329e052c032ea050
Adeet Patel <adeet.patel@gmail.com>
2022-11-29 18:25:39 -0500

    Soft-coding

--- model_evaluate.py
+++ model_evaluate.py
@@ -1,3 +1,5 @@
+# Run as: python model_evaluate.py <input data csv> <trained model>
+
 import pandas as pd
 from CorporateDefaultModel import CorporateDefaultModel
 import dill
@@ -8,7 +10,7 @@ from sklearn.metrics import roc_auc_score
 test = pd.read_csv(sys.argv[1])
 
 # Load the pre-trained model
-filehandler = open("trained_model.obj", 'rb') 
+filehandler = open(sys.argv[2], 'rb')
 model = dill.load(filehandler)
 
 # Use the model to make predictions on the input data
--- model_train.py
+++ model_train.py
@@ -1,15 +1,18 @@
+# Run as: python model_train.py <training data csv> <file to save trained model to>
+
 import pandas as pd
 from CorporateDefaultModel import CorporateDefaultModel
 import dill
 import pickle
+import sys
 
-train = pd.read_csv("train.csv")
+train = pd.read_csv(sys.argv[1])
 
 # Train the model
 model = CorporateDefaultModel()
 model.train_harness(train)
 
 # Save the trained model to a file
-filehandler = open("trained_model.obj", 'wb') 
+filehandler = open(sys.argv[2], 'wb')
 dill.dump(model, filehandler)
 filehandler.close()
